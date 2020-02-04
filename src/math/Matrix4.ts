4aebde3b6154b4be1c2de9ac54122e62274bdff1
FMS-Cat <yutaka.obuchi@gmail.com>
2020-02-04 21:20:29 +0900

    add Matrix4.compose and Matrix4.decompose

--- src/math/Matrix4.ts
+++ src/math/Matrix4.ts
@@ -1,4 +1,4 @@
-import { Vector3 } from '.';
+import { Quaternion, Vector3 } from '.';
 
 export type rawMatrix4 = [
   number, number, number, number,
@@ -36,6 +36,26 @@ export class Matrix4 {
       m[ 2 ], m[ 6 ], m[ 10 ], m[ 14 ],
       m[ 3 ], m[ 7 ], m[ 11 ], m[ 15 ]
     ] );
+  }
+
+  /**
+   * Its determinant.
+   */
+  public get determinant(): number {
+    const m = this.elements;
+    const
+      a00 = m[  0 ], a01 = m[  1 ], a02 = m[  2 ], a03 = m[  3 ],
+      a10 = m[  4 ], a11 = m[  5 ], a12 = m[  6 ], a13 = m[  7 ],
+      a20 = m[  8 ], a21 = m[  9 ], a22 = m[ 10 ], a23 = m[ 11 ],
+      a30 = m[ 12 ], a31 = m[ 13 ], a32 = m[ 14 ], a33 = m[ 15 ],
+      b00 = a00 * a11 - a01 * a10,  b01 = a00 * a12 - a02 * a10,
+      b02 = a00 * a13 - a03 * a10,  b03 = a01 * a12 - a02 * a11,
+      b04 = a01 * a13 - a03 * a11,  b05 = a02 * a13 - a03 * a12,
+      b06 = a20 * a31 - a21 * a30,  b07 = a20 * a32 - a22 * a30,
+      b08 = a20 * a33 - a23 * a30,  b09 = a21 * a32 - a22 * a31,
+      b10 = a21 * a33 - a23 * a31,  b11 = a22 * a33 - a23 * a32;
+
+    return b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
   }
 
   /**
@@ -264,6 +284,81 @@ export class Matrix4 {
       0.0, p, 0.0, 0.0,
       0.0, 0.0, ( far + near ) / d, 1.0,
       0.0, 0.0, -2 * far * near / d, 0.0
+    ] );
+  }
+
+  /**
+   * Decompose this matrix into a position, a scale, and a rotation.
+   * Yoinked from Three.js.
+   */
+  public decompose(): { position: Vector3; scale: Vector3; rotation: Quaternion } {
+    const m = this.elements;
+
+    let sx = new Vector3( [ m[ 0 ], m[ 1 ], m[ 2 ] ] ).length;
+    const sy = new Vector3( [ m[ 4 ], m[ 5 ], m[ 6 ] ] ).length;
+    const sz = new Vector3( [ m[ 8 ], m[ 9 ], m[ 10 ] ] ).length;
+
+    // if determine is negative, we need to invert one scale
+    const det = this.determinant;
+    if ( det < 0 ) { sx = -sx; }
+
+    const invSx = 1.0 / sx;
+    const invSy = 1.0 / sy;
+    const invSz = 1.0 / sz;
+
+    const rotationMatrix = this.clone();
+
+    rotationMatrix.elements[ 0 ] *= invSx;
+    rotationMatrix.elements[ 1 ] *= invSx;
+    rotationMatrix.elements[ 2 ] *= invSx;
+
+    rotationMatrix.elements[ 4 ] *= invSy;
+    rotationMatrix.elements[ 5 ] *= invSy;
+    rotationMatrix.elements[ 6 ] *= invSy;
+
+    rotationMatrix.elements[ 8 ] *= invSz;
+    rotationMatrix.elements[ 9 ] *= invSz;
+    rotationMatrix.elements[ 10 ] *= invSz;
+
+    return {
+      position: new Vector3( [ m[ 12 ], m[ 13 ], m[ 14 ] ] ),
+      scale: new Vector3( [ sx, sy, sz ] ),
+      rotation: Quaternion.fromMatrix( rotationMatrix )
+    };
+  }
+
+  /**
+   * Compose a matrix out of position, scale, and rotation.
+   * Yoinked from Three.js.
+   */
+  public static compose( position: Vector3, rotation: Quaternion, scale: Vector3 ): Matrix4 {
+    const x = rotation.x, y = rotation.y, z = rotation.z, w = rotation.w;
+    const x2 = x + x,	y2 = y + y, z2 = z + z;
+    const xx = x * x2, xy = x * y2, xz = x * z2;
+    const yy = y * y2, yz = y * z2, zz = z * z2;
+    const wx = w * x2, wy = w * y2, wz = w * z2;
+    const sx = scale.x, sy = scale.y, sz = scale.z;
+
+    return new Matrix4( [
+      ( 1.0 - ( yy + zz ) ) * sx,
+      ( xy + wz ) * sx,
+      ( xz - wy ) * sx,
+      0.0,
+
+      ( xy - wz ) * sy,
+      ( 1.0 - ( xx + zz ) ) * sy,
+      ( yz + wx ) * sy,
+      0.0,
+
+      ( xz + wy ) * sz,
+      ( yz - wx ) * sz,
+      ( 1.0 - ( xx + yy ) ) * sz,
+      0.0,
+
+      position.x,
+      position.y,
+      position.z,
+      1.0
     ] );
   }
 }
--- src/math/Quaternion.ts
+++ src/math/Quaternion.ts
@@ -110,4 +110,50 @@ export class Quaternion {
       Math.cos( halfAngle )
     ] );
   }
+
+  /**
+   * Generate a Quaternion out of a rotation matrix.
+   * Yoinked from Three.js.
+   */
+  public static fromMatrix( matrix: Matrix4 ): Quaternion {
+    const m = matrix.elements,
+      m11 = m[ 0 ], m12 = m[ 4 ], m13 = m[ 8 ],
+      m21 = m[ 1 ], m22 = m[ 5 ], m23 = m[ 9 ],
+      m31 = m[ 2 ], m32 = m[ 6 ], m33 = m[ 10 ],
+      trace = m11 + m22 + m33;
+
+    if ( trace > 0 ) {
+      const s = 0.5 / Math.sqrt( trace + 1.0 );
+      return new Quaternion( [
+        ( m32 - m23 ) * s,
+        ( m13 - m31 ) * s,
+        ( m21 - m12 ) * s,
+        0.25 / s
+      ] );
+    } else if ( m11 > m22 && m11 > m33 ) {
+      const s = 2.0 * Math.sqrt( 1.0 + m11 - m22 - m33 );
+      return new Quaternion( [
+        0.25 * s,
+        ( m12 + m21 ) / s,
+        ( m13 + m31 ) / s,
+        ( m32 - m23 ) / s
+      ] );
+    } else if ( m22 > m33 ) {
+      const s = 2.0 * Math.sqrt( 1.0 + m22 - m11 - m33 );
+      return new Quaternion( [
+        ( m12 + m21 ) / s,
+        0.25 * s,
+        ( m23 + m32 ) / s,
+        ( m13 - m31 ) / s
+      ] );
+    } else {
+      const s = 2.0 * Math.sqrt( 1.0 + m33 - m11 - m22 );
+      return new Quaternion( [
+        ( m13 + m31 ) / s,
+        ( m23 + m32 ) / s,
+        0.25 * s,
+        ( m21 - m12 ) / s
+      ] );
+    }
+  }
 }
